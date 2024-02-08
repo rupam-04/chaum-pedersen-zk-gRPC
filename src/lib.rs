a382f26b7bbff18a1690915a804c32944fa82ae2
rupam-04 <rpmdey2004@gmail.com>
2024-02-09 00:37:18 +0530

    Exponentiating, Solving and Verifying

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use num_bigint::BigUint;
+
+pub fn exponentiate(n: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {   // n^exp mod p
+    n.modpow(exponent, modulus)
+}
+
+pub fn solve(k: &BigUint, c: &BigUint, x: &BigUint, q: &BigUint) -> BigUint {   //s = k-cx mod q
+    if *k >= c * x {
+        return (k - c * x).modpow(&BigUint::from(1u32), q);
+    }
+    return (q + k - c * x).modpow(&BigUint::from(1u32), q);
+}
+
+pub fn verify(r1: &BigUint, r2: &BigUint, y1: &BigUint, y2: &BigUint, alpha: &BigUint, beta: &BigUint, c: &BigUint, s: &BigUint, p: &BigUint) -> bool {
+    let cond1 = *r1 == alpha.modpow(s, p) * y1.modpow(c, p);
+    let cond2 = *r2 == alpha.modpow(s, p) * y2.modpow(c, p);
+    cond1 && cond2
 }
