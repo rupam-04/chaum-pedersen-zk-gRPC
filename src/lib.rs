cf276646f081c3b546a6bed835d1808be0131a46
rupam-04 <rpmdey2004@gmail.com>
2024-02-09 15:39:30 +0530

    Added tests

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,55 @@ pub fn solve(k: &BigUint, c: &BigUint, x: &BigUint, q: &BigUint) -> BigUint {   
     if *k >= c * x {
         return (k - c * x).modpow(&BigUint::from(1u32), q);
     }
-    return (q + k - c * x).modpow(&BigUint::from(1u32), q);
+    return (q - (c * x - k).modpow(&BigUint::from(1u32), q)).modpow(&BigUint::from(1u32), q);
 }
 
 pub fn verify(r1: &BigUint, r2: &BigUint, y1: &BigUint, y2: &BigUint, alpha: &BigUint, beta: &BigUint, c: &BigUint, s: &BigUint, p: &BigUint) -> bool {
-    let cond1 = *r1 == alpha.modpow(s, p) * y1.modpow(c, p);
-    let cond2 = *r2 == alpha.modpow(s, p) * y2.modpow(c, p);
+    let cond1 = *r1 == (alpha.modpow(s, p) * y1.modpow(c, p)).modpow(&BigUint::from(1u32), p);
+    let cond2 = *r2 == (beta.modpow(s, p) * y2.modpow(c, p)).modpow(&BigUint::from(1u32), p);
     cond1 && cond2
+}
+
+#[cfg(test)]
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_toy_example() {
+        let alpha = BigUint::from(4u32);
+        let beta = BigUint::from(9u32);
+        let p = BigUint::from(23u32);
+        let q = BigUint::from(11u32);
+
+        let x = BigUint::from(6u32);
+        let k = BigUint::from(7u32);
+        
+        let c = BigUint::from(4u32);
+
+        let y1 = exponentiate(&alpha, &x, &p);
+        let y2 = exponentiate(&beta, &x, &p);
+
+        assert_eq!(y1, BigUint::from(2u32));
+        assert_eq!(y2, BigUint::from(3u32));
+
+        let r1 = exponentiate(&alpha, &k, &p);
+        let r2 = exponentiate(&beta, &k, &p);
+
+        assert_eq!(r1, BigUint::from(8u32));
+        assert_eq!(r2, BigUint::from(4u32));
+
+        let s = solve(&k, &c, &x, &q);
+        assert_eq!(s, BigUint::from(5u32));
+
+        let result = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s, &p);
+        assert!(result);
+
+        // fake secret
+        let x_fake = BigUint::from(7u32);
+        let s_fake = solve(&k, &c, &x_fake, &q);
+
+        let result_fake = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s_fake, &p);
+        assert!(!result_fake);
+    }
 }
