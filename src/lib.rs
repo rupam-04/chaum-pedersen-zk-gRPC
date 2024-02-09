d178cd33c0851fdccbe3e6854200b6d8fe08da95
rupam-04 <rpmdey2004@gmail.com>
2024-02-09 16:39:54 +0530

    Added random number and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use num_bigint::BigUint;
+use num_bigint::{BigUint, RandBigInt};
+use rand;
 
 pub fn exponentiate(n: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {   // n^exp mod p
     n.modpow(exponent, modulus)
@@ -15,6 +16,11 @@ pub fn verify(r1: &BigUint, r2: &BigUint, y1: &BigUint, y2: &BigUint, alpha: &Bi
     let cond1 = *r1 == (alpha.modpow(s, p) * y1.modpow(c, p)).modpow(&BigUint::from(1u32), p);
     let cond2 = *r2 == (beta.modpow(s, p) * y2.modpow(c, p)).modpow(&BigUint::from(1u32), p);
     cond1 && cond2
+}
+
+pub fn generate_random_below(bound: &BigUint) -> BigUint {
+    let mut rng = rand::thread_rng();
+    rng.gen_biguint_below(bound)
 }
 
 #[cfg(test)]
@@ -58,5 +64,40 @@ mod test {
 
         let result_fake = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s_fake, &p);
         assert!(!result_fake);
+    }
+
+    #[test]
+
+    fn test_toy_example_with_random_numbers() {
+        let alpha = BigUint::from(4u32);
+        let beta = BigUint::from(9u32);
+        let p = BigUint::from(23u32);
+        let q = BigUint::from(11u32);
+
+        let x = BigUint::from(6u32);
+        let k = generate_random_below(&q);
+        
+        let c = generate_random_below(&q);
+
+        let y1 = exponentiate(&alpha, &x, &p);
+        let y2 = exponentiate(&beta, &x, &p);
+
+        assert_eq!(y1, BigUint::from(2u32));
+        assert_eq!(y2, BigUint::from(3u32));
+
+        let r1 = exponentiate(&alpha, &k, &p);
+        let r2 = exponentiate(&beta, &k, &p);
+
+        let s = solve(&k, &c, &x, &q);
+
+        let result = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s, &p);
+        assert!(result);
+
+        // fake secret
+        // let x_fake = BigUint::from(7u32);
+        // let s_fake = solve(&k, &c, &x_fake, &q);
+
+        // let result_fake = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s_fake, &p);
+        // assert!(!result_fake);
     }
 }
